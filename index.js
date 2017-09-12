15dd18d8feeb2b2df9889f02b930e36d42b5211e
Matt Latimer <matt.latimer@gmail.com>
2017-09-12 16:38:36 -0700

    new urls page is posting to urls endpoint

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
-var urlDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -13,14 +15,24 @@ app.get("/", (req, res) => {
   res.end("Hello!");
 });
 
-app.get ('/urls/:id', (req, res) => {
-  let templateVars = { shortURL: req.params.id, urls: urlDatabase };
-  res.render('urls-show', templateVars);
-});
-
-app.get ('/urls', (req, res) => {
+app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase};
   res.render('urls-index', templateVars);
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('ok');
+});
+
+app.get('/urls/new', (req, res) => {
+  let templateVars = { urls: urlDatabase};
+  res.render('urls-new', templateVars);
+});
+
+app.get('/urls/:id', (req, res) => {
+  let templateVars = { shortURL: req.params.id, urls: urlDatabase };
+  res.render('urls-show', templateVars);
 });
 
 app.get('/urls.json', (req, res) => {
