23f3c62654d7d2127122d4c14580a5f708782268
Matt Latimer <matt.latimer@gmail.com>
2017-09-14 21:03:10 -0700

    bug fix

--- index.js
+++ index.js
@@ -145,7 +145,7 @@ app.post('/register', (req, res) => {
 
 app.get('/u/:id', (req, res) => {
   if (urlDatabase[req.params.id]) {
-    res.redirect(301, urlDatabase[req.params.shortURL].url);
+    res.redirect(301, urlDatabase[req.params.id].url);
   } else {
     res.redirect('/error/noLink');
   }
